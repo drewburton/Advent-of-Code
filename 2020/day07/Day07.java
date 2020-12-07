45fd230fb5c1c7cf7a2131a3bf1ce1130f24f9b0
Drew Burton <drewburton@users.noreply.github.com>
2020-12-07 14:35:33 -0500

    finished day 7 part 2

--- 2020/day07/Day07.java
+++ 2020/day07/Day07.java
@@ -10,6 +10,7 @@ public class Day07 {
     public Day07() {
         try {
             Scanner scanner = new Scanner(new File("day07\\day7input.txt"));
+            // Scanner scanner = new Scanner(new File("day07\\test.txt"));
             rules = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 rules.add(scanner.nextLine());
@@ -25,37 +26,50 @@ public class Day07 {
             parseRule(rule);
         }
 
-        HashSet<String> colors = new HashSet<>();
+        System.out.println(getBagsInShinygold("1shinygoldbag") - 1);
 
-        for (String b : bags.keySet()) {
-            colors.addAll(getColorsWithShinyGold(b));
-        }
+        // HashSet<String> colors = new HashSet<>();
 
-        System.out.println(colors.size());
+        // for (String b : bags.keySet()) {
+        //     colors.addAll(getColorsWithShinyGold(b));
+        // }
+
+        // System.out.println(colors.size());
     }
 
-    private HashSet<String> getColorsWithShinyGold(String b) {
-        HashSet<String> colors = new HashSet<>();
-        boolean containsGold = false;
-        for (String bag : bags.get(b)) {
-            if ("shinygoldbag".equals(bag))
-                containsGold = true;
-            colors.addAll(getColorsWithShinyGold(bag));
+    // private HashSet<String> getColorsWithShinyGold(String b) {
+    //     HashSet<String> colors = new HashSet<>();
+    //     boolean containsGold = false;
+    //     for (String bag : bags.get(b)) {
+    //         if ("shinygoldbag".equals(bag))
+    //             containsGold = true;
+    //         colors.addAll(getColorsWithShinyGold(bag));
+    //     }
+    //     if (containsGold || colors.size() > 0) {
+    //         colors.add(b);
+    //     }
+    //     return colors;
+    // }
+
+    private int getBagsInShinygold(String bag) {
+        int count = 0;
+        for (String s : bags.get(bag.substring(1))) {
+            if (!"nootherbag".equals(s))
+                count += getBagsInShinygold(s);
         }
-        if (containsGold || colors.size() > 0) {
-            colors.add(b);
-        }
-        return colors;
+        if (!"nootherbag".equals(bags.get(bag.substring(1)).get(0)))
+            return count * Integer.parseInt(bag.substring(0, 1)) + Integer.parseInt(bag.substring(0, 1));
+        return Integer.parseInt(bag.substring(0, 1));
     }
 
     private void parseRule(String rule) {
         rule = rule.replaceAll(" ", "");
         String[] parts = rule.split("contains{0,1}");
         String bag = parts[0].substring(0, parts[0].length() - 1);
-        String[] insideBags = parts[1].split("s*,*[0-9]+|s*\\.");
+        String[] insideBags = parts[1].split("s*,|s*\\.");
 
         ArrayList<String> bags = new ArrayList<>();
-        for (int i = 1; i < insideBags.length; i++) {
+        for (int i = 0; i < insideBags.length; i++) {
             bags.add(insideBags[i]);
         }
         this.bags.put(bag, bags);
